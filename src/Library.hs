c938c6e701898002a8ffbb4b72049aaf06e1e4fb
jabrampuga-eng <jabrampuga@frba.utn.edu.ar>
2026-05-17 12:17:48 -0300

    Terminado

--- src/Library.hs
+++ src/Library.hs
@@ -2,19 +2,116 @@ module Library where
 import PdePreludat
 
 data Ingrediente =
-    Carne | Pan | Panceta | Cheddar | Pollo | Curry | QuesoDeAlmendras
+    Carne | Pan | Panceta | Cheddar | Pollo | Curry
+    | QuesoDeAlmendras | BaconDeTofu
+    | PatiVegano | Papas | PanIntegral
     deriving (Eq, Show)
 
+precioIngrediente :: Ingrediente -> Number
 precioIngrediente Carne = 20
 precioIngrediente Pan = 2
 precioIngrediente Panceta = 10
 precioIngrediente Cheddar = 10
-precioIngrediente Pollo =  10
+precioIngrediente Pollo = 10
 precioIngrediente Curry = 5
 precioIngrediente QuesoDeAlmendras = 15
+precioIngrediente BaconDeTofu = 12
+precioIngrediente PatiVegano = 10
+precioIngrediente Papas = 10
+precioIngrediente PanIntegral = 3
 
 data Hamburguesa = Hamburguesa {
     precioBase :: Number,
     ingredientes :: [Ingrediente]
 } deriving (Eq, Show)
 
+
+precioFinal :: Hamburguesa -> Number
+precioFinal hamburguesa =
+    precioBase hamburguesa +
+    sum (map precioIngrediente (ingredientes hamburguesa))
+
+
+cuartoDeLibra :: Hamburguesa
+cuartoDeLibra =
+    Hamburguesa 20 [Pan, Carne, Cheddar, Pan]
+
+
+contiene :: Ingrediente -> Hamburguesa -> Bool
+contiene ingrediente =
+    any (== ingrediente) . ingredientes
+
+agregarIngrediente :: Ingrediente -> Hamburguesa -> Hamburguesa
+agregarIngrediente ingrediente hamburguesa =
+    hamburguesa {
+        ingredientes =
+            ingrediente : ingredientes hamburguesa
+    }
+
+descuento :: Number -> Hamburguesa -> Hamburguesa
+descuento porcentaje hamburguesa =
+    hamburguesa {
+        precioBase =
+            precioBase hamburguesa *
+            (1 - porcentaje / 100)
+    }
+
+agrandar :: Hamburguesa -> Hamburguesa
+agrandar hamburguesa
+    | contiene Carne hamburguesa =
+        agregarIngrediente Carne hamburguesa
+
+    | contiene Pollo hamburguesa =
+        agregarIngrediente Pollo hamburguesa
+
+    | otherwise =
+        agregarIngrediente PatiVegano hamburguesa
+
+pdepBurger :: Hamburguesa
+pdepBurger =
+    (descuento 20 . (agregarIngrediente Cheddar .(agregarIngrediente Panceta .(agrandar . agrandar))))
+    cuartoDeLibra
+
+dobleCuarto :: Hamburguesa
+dobleCuarto =
+    (agregarIngrediente Cheddar . agregarIngrediente Carne)
+    cuartoDeLibra
+
+bigPdep :: Hamburguesa
+bigPdep =
+    agregarIngrediente Curry dobleCuarto
+
+delDia :: Hamburguesa -> Hamburguesa
+delDia =
+    descuento 30 .
+    agregarIngrediente Papas
+
+hacerVeggie :: Hamburguesa -> Hamburguesa
+hacerVeggie hamburguesa =
+    hamburguesa {
+        ingredientes =
+            map reemplazarIngrediente
+            (ingredientes hamburguesa)
+    }
+
+reemplazarIngrediente :: Ingrediente -> Ingrediente
+reemplazarIngrediente Carne = PatiVegano
+reemplazarIngrediente Pollo = PatiVegano
+reemplazarIngrediente Cheddar = QuesoDeAlmendras
+reemplazarIngrediente Panceta = BaconDeTofu
+reemplazarIngrediente ingrediente = ingrediente
+
+cambiarPanDePati :: Hamburguesa -> Hamburguesa
+cambiarPanDePati hamburguesa =
+    hamburguesa {
+        ingredientes =
+            map cambiarPan
+            (ingredientes hamburguesa)
+    }
+
+cambiarPan :: Ingrediente -> Ingrediente
+cambiarPan Pan = PanIntegral
+cambiarPan ingrediente = ingrediente
+
+dobleCuartoVegano :: Hamburguesa
+dobleCuartoVegano = (cambiarPanDePati . hacerVeggie) dobleCuarto
--- src/Spec.hs
+++ src/Spec.hs
@@ -2,10 +2,81 @@ module Spec where
 import PdePreludat
 import Library
 import Test.Hspec
-import Control.Exception (evaluate)
 
 correrTests :: IO ()
 correrTests = hspec $ do
     describe "TP 5" $ do
-        it "test de prueba" $ do
-            2 + 2 `shouldBe` 4
+
+        describe "precioFinal" $ do
+            it "el precio final de un cuarto de libra es 54" $ do
+                precioFinal cuartoDeLibra `shouldBe` 54
+
+        describe "agregarIngrediente" $ do
+            it "agrega un ingrediente a la hamburguesa" $ do
+                ingredientes
+                    (agregarIngrediente Panceta cuartoDeLibra)
+                    `shouldBe`
+                    [Panceta, Pan, Carne, Cheddar, Pan]
+
+        describe "descuento" $ do
+            it "aplica descuento sobre el precio base" $ do
+                precioBase
+                    (descuento 20 cuartoDeLibra)
+                    `shouldBe`
+                    16
+
+        describe "agrandar" $ do
+            it "si tiene carne agrega otra carne" $ do
+                ingredientes
+                    (agrandar cuartoDeLibra)
+                    `shouldBe`
+                    [Carne, Pan, Carne, Cheddar, Pan]
+
+            it "si tiene pollo agrega pollo" $ do
+                ingredientes
+                    (agrandar (Hamburguesa 10 [Pan, Pollo]))
+                    `shouldBe`
+                    [Pollo, Pan, Pollo]
+
+        describe "pdepBurger" $ do
+            it "vale 110" $ do
+                precioFinal pdepBurger `shouldBe` 110
+
+        describe "dobleCuarto" $ do
+            it "vale 84" $ do
+                precioFinal dobleCuarto `shouldBe` 84
+
+        describe "bigPdep" $ do
+            it "vale 89" $ do
+                precioFinal bigPdep `shouldBe` 89
+
+        describe "delDia" $ do
+            it "una doble cuarto del dia vale 88" $ do
+                precioFinal (delDia dobleCuarto)
+                    `shouldBe`
+                    88
+
+        describe "hacerVeggie" $ do
+            it "reemplaza carne por pati vegano" $ do
+                ingredientes
+                    (hacerVeggie cuartoDeLibra)
+                    `shouldBe`
+                    [Pan, PatiVegano, QuesoDeAlmendras, Pan]
+
+        describe "cambiarPanDePati" $ do
+            it "cambia pan por pan integral" $ do
+                ingredientes
+                    (cambiarPanDePati cuartoDeLibra)
+                    `shouldBe`
+                    [PanIntegral, Carne, Cheddar, PanIntegral]
+
+        describe "dobleCuartoVegano" $ do
+            it "es veggie y con pan integral" $ do
+                ingredientes dobleCuartoVegano
+                    `shouldBe`
+                    [QuesoDeAlmendras,
+                     PatiVegano,
+                     PanIntegral,
+                     PatiVegano,
+                     QuesoDeAlmendras,
+                     PanIntegral]
